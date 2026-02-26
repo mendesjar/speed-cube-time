f7b95162b25e6dcb266658d0eb4953b1b4f44854
mendesjar <ga0301@outlook.com>
2026-02-26 15:15:54 -0300

    fix: ajustar estrutura do componente Header

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -47,7 +47,7 @@ export function Header({
               <img src={LogoMarca} alt="Logo Marca" />
             </a>
           </div>
-          <div className="fixed inset-x-0 top-0 z-10 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80 backdrop-blur-sm dark:backdrop-blur bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]">
+          <div className="fixed inset-x-0 top-0 z-10 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80">
             <div className="absolute inset-x-0 top-full h-px transition bg-zinc-900/7.5 dark:bg-white/7.5"></div>
             <div className="select-none w-full flex items-center justify-between gap-5 lg:hidden">
               <a aria-label="Home" href="/">
